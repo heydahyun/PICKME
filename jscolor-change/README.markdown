84f452e7d017377f979c503442cd907308906ab4
heydahyun <hwangdahyun0715@gmail.com>
2022-01-22 19:33:08 +0100

    deleting

--- jscolor-change/README.markdown
+++
@@ -1,5 +0,0 @@
-# JS - Color change
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/cat999/pen/oNYmWJb](https://codepen.io/cat999/pen/oNYmWJb).
-
-
--- jscolor-change/dist/script.js
+++
@@ -1,8 +0,0 @@
-const bodyTag = document.querySelector("body")
-
-const colorButton = document.querySelector(`input[type="color"]`)
-
-colorButton.addEventListener("input", function() {
-  bodyTag.style.backgroundColor = this.value
-
-});
